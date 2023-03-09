21866674edcc5078a90e75eccc55053e25cb229f
chioma <chiomag58@gmail.com>
2023-03-09 15:03:42 +0100

    final update

--- lib/Phone_folder/phone_check.dart
+++ lib/Phone_folder/phone_check.dart
@@ -3,99 +3,91 @@ import 'package:flutter_svg/svg.dart';
 import 'phone_details.dart';
 import 'package:email_validator/email_validator.dart';
 
-class phonecheck extends StatelessWidget {
+class phonecheck extends StatefulWidget {
   const phonecheck({super.key});
+
+  @override
+  State<phonecheck> createState() => _phonecheckState();
+}
+
+class _phonecheckState extends State<phonecheck> {
+  final phoneNumberController = TextEditingController();
+  final phoneFormKey = GlobalKey<FormState>();
+  void selectSubmit() {
+    if (phoneNumberController.text.isNotEmpty &&
+        phoneFormKey.currentState!.validate() &&
+        !(phoneNumberController.text.length < 10)) {
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+        return phoneNumberDetails(phoneNumberController.text);
+      }));
+    }
+  }
+
+  void resultOutput(int output) {
+    debugPrint('Phone Number: $output');
+  }
+
+  @override
+  void dispose() {
+    phoneNumberController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Phone Number checker')),
       body: Padding(
           padding: EdgeInsets.all(10),
           child: Expanded(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Center(
                     child: SvgPicture.asset(
-                  'images/email_opened.svg',
-                  height: 150,
-                  width: 150,
+                  'images/phone_opened.svg',
+                  height: 120,
+                  width: 120,
                 )),
                 SizedBox(
                   height: 10,
                 ),
-                Textfield(),
+                Form(
+                  key: phoneFormKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty && value.length < 10) {
+                        return 'phone number (+x xxx xxx xxxx)';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        filled: true,
+                        icon: Icon(Icons.phone),
+                        hintText: 'Your phone number',
+                        labelText: 'Phone no'),
+                    onFieldSubmitted: (value) {
+                      return resultOutput(value as int);
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    controller: phoneNumberController,
+                  ),
+                ),
                 SizedBox(
                   height: 10,
                 ),
-                Submitbar(),
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: selectSubmit, child: Text('SUBMIT'))
+                  ],
+                ),
               ],
             ),
           )),
-    );
-  }
-}
-
-class Textfield extends StatefulWidget {
-  const Textfield({super.key});
-
-  @override
-  State<Textfield> createState() => _TextfieldState();
-}
-
-class _TextfieldState extends State<Textfield> {
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: phoneFormKey,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      child: TextFormField(
-        validator: (value) {
-          if (value!.isEmpty && value.length < 10) {
-            return 'phone number (+x xxx xxx xxxx)';
-          }
-        },
-        decoration: InputDecoration(
-            border: UnderlineInputBorder(),
-            filled: true,
-            icon: Icon(Icons.email),
-            hintText: 'Your phone number',
-            labelText: 'Phone no'),
-        onChanged: (value) {
-          String phoneNumber = value;
-        },
-        keyboardType: TextInputType.emailAddress,
-        controller: phoneNumberController,
-      ),
-    );
-  }
-}
-
-class Submitbar extends StatefulWidget {
-  const Submitbar({super.key});
-
-  @override
-  State<Submitbar> createState() => _SubmitbarState();
-}
-
-final phoneNumberController = TextEditingController();
-final phoneFormKey = GlobalKey<FormState>();
-
-class _SubmitbarState extends State<Submitbar> {
-  void selectSubmit() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-      return phoneNumberDetails(phoneNumberController.text);
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ButtonBar(
-      alignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ElevatedButton(onPressed: selectSubmit, child: Text('SUBMIT'))
-      ],
     );
   }
 }
